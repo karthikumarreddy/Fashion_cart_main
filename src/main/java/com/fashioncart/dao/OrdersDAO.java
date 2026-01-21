23209f25cb992f03b469104de706239b54a8368e
karthikumarreddy <reddykarthikumar@gmail.com>
2026-01-21 12:12:05 +0530

    created order and order dao by karthi

---
+++ src/main/java/com/fashioncart/dao/OrdersDAO.java
@@ -0,0 +1,13 @@
+package com.fashioncart.dao;
+
+import java.util.List;
+
+import util.Orders;
+
+
+
+public class OrdersDAO {
+	List<Orders> orderList=null;
+	
+
+}
---
+++ src/main/java/util/Orders.java
@@ -0,0 +1,36 @@
+package util;
+
+public class Orders {
+	private String orderId;
+	private double totalAmount;
+	private String paymentMode;
+	private String stats;
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+	public String getStats() {
+		return stats;
+	}
+	public void setStats(String stats) {
+		this.stats = stats;
+	}
+	
+	
+	
+
+}
