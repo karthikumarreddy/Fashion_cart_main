57455e39a50256afda08ee731a9b18cbc43de23e
Vigneshvvigneshv <vigneshvvigneshv25@gmail.com>
2026-01-21 10:01:24 +0530

    Save local changes

--- src/main/java/com/fashioncart/command/AddToCartCommand.java
+++ src/main/java/com/fashioncart/command/AddToCartCommand.java
@@ -16,27 +16,21 @@ public class AddToCartCommand implements Command {
 
 	@Override
 	public boolean execute(HttpServletRequest req, HttpServletResponse res) {
-		HttpSession session=null;
+
 		List<Product> cartList = null;
 		ProductDAO product = new ProductDAO();
-		String product_id = req.getParameter("id");
-		System.out.println("from add to cart"+product_id);
+		String product_id = req.getParameter("product_id");
 		Product cartProduct = product.getProductById(product_id);
 		if (cartList == null) {
 			cartList = new ArrayList<Product>();
-			
+
 		}
-		if(session==null) {
-			session=req.getSession();
-		}
+		HttpSession session=req.getSession();
 		
-	
 		cartList.add(cartProduct);
-		System.out.println(cartList);
 		session.setAttribute("cartList", cartList);
 		Gson gson=new Gson();
 		String json=gson.toJson(cartList);
-		System.out.println(json);
 		try {
 			res.getWriter().print(json);
 		} catch (IOException e) {
